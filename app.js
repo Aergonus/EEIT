843be49ff6d80cd393b45d92d9055a82ec732708
Spellchaser <KangqiaoLei@gmail.com>
2016-12-16 12:45:32 -0500

    Secretize secrets
    Also fixed some inital booboos

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-var env = process.env.NODE_ENV || 'admin';
-var config = require('./config')[env];
-
-/* Development command line prompt
-var prompt     = require('prompt'); 
-var schema     = {
-	properties: {
-		user: {
-			pattern: /^[a-zA-Z\s\-]+$/,
-			description: 'Username for Database Access',
-			message: 'Only different for Administrative Access',
-			default: 'user',
-			required: true
-		},
-		password: {
-			description: 'Enter generic EE student password',
-			type: 'string', 
-			pattern: /^[\w!?]+$/, 
-			hidden: true,
-			replace: '*', 
-			required: true
-		}
-	}
-};
-prompt.start();
-prompt.get(schema, function (err, result) {
-	connectionparams.user = result.user;
-	connectionparams.password = result.password;
-});
-*/
+var env        = process.env.NODE_ENV || 'admin';
+var config     = require('./config')[env];
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection(config);
